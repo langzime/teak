c1573e7f3832acb30039b2561d174f61db8335f7
codekitten <codekitten@126.com>
2013-03-13 09:02:00 +0800

    Signed-off-by: codekitten <codekitten@126.com>

--- src/main/java/im/dadoo/teak/util/Util.java
+++ src/main/java/im/dadoo/teak/util/Util.java
@@ -7,7 +7,7 @@ public final class Util {
 	public static final String MENU_SESSION_NAME = "menu";
 	
 	public static String parse(String html) {
-		String result = Jsoup.parse(html).text().replaceAll("&nbsp;", " ");
+		String result = Jsoup.parse(html).text();
 		System.out.println(result);
 		return result;
 	}
